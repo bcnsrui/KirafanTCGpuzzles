d60f0b01641e41b63c41f607e2cbbb5a3368f5d9
bcnsrui <bcnsuri@naver.com>
2024-08-13 23:43:52 +0900

    튜토리얼 완료

--- 3.승려.lua
+++ 3.승려.lua
@@ -27,7 +27,6 @@ Debug.AddCard(10052222,1,1,LOCATION_MZONE,4,POS_FACEUP)
 Debug.AddCard(10052222,1,1,LOCATION_MZONE,4,POS_FACEUP)
 
 Debug.AddCard(10054511,0,0,LOCATION_GRAVE,0,POS_FACEUP)
-Debug.AddCard(10054512,0,0,LOCATION_GRAVE,0,POS_FACEUP)
 Debug.AddCard(10054621,0,0,LOCATION_DECK,0,POS_FACEDOWN)
 Debug.AddCard(10054621,0,0,LOCATION_DECK,0,POS_FACEDOWN)
 Debug.AddCard(10054621,0,0,LOCATION_DECK,0,POS_FACEDOWN)
@@ -45,9 +44,7 @@ Debug.AddCard(10054621,1,1,LOCATION_DECK,0,POS_FACEDOWN)
 Debug.AddCard(10054621,1,1,LOCATION_DECK,0,POS_FACEDOWN)
 
 
-kana=Debug.AddCard(10051421,0,0,LOCATION_MZONE,2,POS_FACEUP_ATTACK)
-Debug.AddCard(10052421,0,0,LOCATION_MZONE,2,POS_FACEUP)
-
+kana=Debug.AddCard(10051421,0,0,LOCATION_HAND,0,POS_FACEDOWN)
 miku=Debug.AddCard(10054111,0,0,LOCATION_HAND,0,POS_FACEDOWN)
 Debug.AddCard(10054321,0,0,LOCATION_HAND,0,POS_FACEDOWN)
 Debug.AddCard(10054421,0,0,LOCATION_HAND,0,POS_FACEDOWN)
@@ -107,6 +104,65 @@ Debug.ShowHint[[★우시쿠 카나
 그렇게 불러도되는건 선배뿐이야!!]]
 Debug.ShowHint[[★클레어
 이.. 일단 저희는 카나 씨를 돕도록 해볼까요?]]
+
+
+
+
+local e1=Effect.GlobalEffect()
+e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+e1:SetCode(EVENT_TURN_END)
+e1:SetCountLimit(1)
+e1:SetOperation(function(e,tp,eg,ep,ev,re,r,rp)
+if Duel.GetMatchingGroupCount(nil,tp,0,LOCATION_MZONE,nil)==2 then
+Debug.ShowHint[[★클레어
+자, 그럼 다음엔
+게임에서 승리하는 방법에 대해 알아보도록 해요!]]
+Duel.SetLP(1-tp,0)
+else
+Duel.SetLP(0,0)
+end
+end)
+Duel.RegisterEffect(e1,0)
+
+local e2=Effect.GlobalEffect()
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e2:SetCode(EVENT_PHASE+PHASE_DRAW)
+	e2:SetCountLimit(1)
+	e2:SetOperation(function(e,tp,eg,ep,ev,re,r,rp)
+Debug.ShowHint[[★클레어
+지금은 드로우 페이즈에요!
+드로우 페이즈에 턴 플레이어는 2장을 드로우하는데
+서로의 첫 턴에는 드로우하지 않아요]]
+Debug.ShowHint[[★클레어
+그리고 메인 캐릭터의 최대 성채석이
+양플레이어 서로 1개씩 늘어나요!
+지금은 제가 성채석을 1개 가져왔으니 그걸로 늘어났다고 칠게요]]
+Debug.ShowHint[[★클레어
+다시 말해서 서로의 첫 턴 이후부터는 사용가능 성채석이
+선공 플레이어는 자신 턴에 홀수 성채석을
+후공 플레이어는 자신 턴에 짝수 성채석을 가지게 되는거죠]]
+Debug.ShowHint[[★클레어
+드로우 페이즈가 지나면 스탠바이 페이즈가 돼요!
+스탠바이 페이즈에는 덱 위에서 카드 1장을 돗테오키 존으로 보내요
+카드 효과에서는 간단하게 돗테오키 게이지+1 이라고 표현된답니다]]
+Debug.ShowHint[[★클레어
+그리고 패나 배틀 존에서 카드 1장을 돗테오키 존으로 보낼 수 있어요
+배틀 존에서 보냈을 땐 그 아래 있던 체력 카드는 전부 마을로 보내죠
+서로의 첫 턴엔 할 수 없으니 지금은 넘어갈게요]]
+Debug.ShowHint[[★클레어
+마지막으로 배틀 존에 크리에메이트가 하나도 없다면
+배틀 존에 크리에메이트를 하나 소환해야만해요
+만약 소환하지 못하면 자신의 패배가 되니 주의해주세요!]]
+Debug.ShowHint[[★클레어
+지금은 마침 패에 카나 씨가 있으니 카나 씨를 소환해볼까요?]]
+end)
+Duel.RegisterEffect(e2,0)
+
+local e5=Effect.GlobalEffect()
+	e5:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e5:SetCode((EVENT_SPSUMMON_SUCCESS))
+	e5:SetCountLimit(1)
+	e5:SetOperation(function(e,tp,eg,ep,ev,re,r,rp)
 Debug.ShowHint[[★클레어
 현재 제가 가진 성채석은 3개인데
 이미 필드에 우시쿠 카나 씨가 있어서 2개는 사용중이에요]]
@@ -117,12 +173,14 @@ Debug.ShowHint[[★클레어
 일단 할 수 있는걸 하죠!
 먼저 미쿠 씨의 공격력 증가 효과를 카나 씨에게 사용하는거예요
 목표는 코테츠 씨만 제외하고 필드를 정리하는걸로 하죠]]
+end)
+Duel.RegisterEffect(e5,0)
 
-local e2=Effect.GlobalEffect()
-	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-	e2:SetCode(EVENT_PHASE+PHASE_BATTLE_START)
-	e2:SetCountLimit(1)
-	e2:SetOperation(function(e,tp,eg,ep,ev,re,r,rp)
+local e3=Effect.GlobalEffect()
+	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e3:SetCode(EVENT_PHASE+PHASE_BATTLE_START)
+	e3:SetCountLimit(1)
+	e3:SetOperation(function(e,tp,eg,ep,ev,re,r,rp)
 if miku:IsLocation(LOCATION_SZONE) and kana:GetAttack()>2 then
 Debug.ShowHint[[★클레어
 잘하셨어요 이렇게 승려 크리에메이트는 서포트 존에 나오게돼요
@@ -155,7 +213,8 @@ Debug.ShowHint[[★클레어
 아앗! 혹시 실수하신건가요?
 어째서 미쿠 씨의 회복 효과를 사용하신건가요?]]
 Debug.ShowHint[[★클레어
-카나 씨의 체력이 1이라서 회복한 건 좋았지만...]]
+카나 씨는 이미 최대 체력이라 일반적인 회복으론
+회복을 못 해요...]]
 Debug.ShowHint[[★하토야 코하네
 아니야! 내가 하나와쨩의 공격력을 올려줄게
 Go! Fight! win!
@@ -180,7 +239,7 @@ Debug.ShowHint[[★클레어
 Duel.SetLP(tp,0)
 end
 end)
-Duel.RegisterEffect(e2,0)
+Duel.RegisterEffect(e3,0)
 
 local e4=Effect.GlobalEffect()
 	e4:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
@@ -193,9 +252,17 @@ Debug.ShowHint[[★클레어
 이런 식으로 문제를 해결하는 것
 역시 이 게임에서 중요하답니다]]
 Debug.ShowHint[[★클레어
-자, 그럼 다음엔
-게임에서 승리하는 방법에 대해 알아보도록 해요!]]
-Duel.SetLP(1-tp,0)
+미쿠 씨의 공격력 증가 효과는 배틀 페이즈 종료시에
+드로우를 1장 보는 보너스 효과가 있어요!
+최소한 손해는 보지 않는 효과라는 소리죠]]
+Debug.ShowHint[[★클레어
+이런 효과는 사용하기만 하면 이득이지만
+성채석 제한은 물론 서포트 존은 2개밖에 없고 자신 턴 종료시에
+패가 6장 이상이면 5장이 되도록 마을로 보내는 걸 생각해주세요]]
+Debug.ShowHint[[★클레어
+마지막으로 이번 턴에 사용한
+양쪽 플레이어의 서포트 크리에메이트는
+턴 종료시에 전부 마을로 보내져요]]
 elseif kotetsu:IsLocation(LOCATION_EXTRA) then
 Debug.ShowHint[[★타테지마 코테츠
 어째서?]]
@@ -220,5 +287,3 @@ end)
 Duel.RegisterEffect(e4,0)
 
 
-
-aux.BeginPuzzle()
