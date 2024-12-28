e8dcec0787f02e518f2556181cc0fc27635b7c2e
bcnsrui <bcnsuri@naver.com>
2024-12-28 21:56:47 +0900

    퍼즐듀얼 추가

--- 한국어 심화편/6.스텔라의 마법.lua
+++ 한국어 심화편/6.스텔라의 마법.lua
@@ -5,43 +5,37 @@ Debug.SetPlayerInfo(0,8000,0,0)
 Debug.SetPlayerInfo(1,8000,0,0)
 
 --클레어
-c=Debug.AddCard(10050111,0,0,LOCATION_EMZONE,1,POS_FACEUP_ATTACK)
-tc=Debug.AddCard(10050113,1,1,LOCATION_EMZONE,1,POS_FACEUP_ATTACK)
+c=Debug.AddCard(10050110,0,0,LOCATION_EMZONE,1,POS_FACEUP_ATTACK)
+tc=Debug.AddCard(10050114,1,1,LOCATION_EMZONE,1,POS_FACEUP_ATTACK)
 
-Debug.AddCard(10051531,1,1,LOCATION_MZONE,2,POS_FACEUP_ATTACK)
-Debug.AddCard(10051531,1,1,LOCATION_MZONE,2,POS_FACEUP)
-Debug.AddCard(10051531,1,1,LOCATION_MZONE,2,POS_FACEUP)
-Debug.AddCard(10051531,1,1,LOCATION_MZONE,2,POS_FACEUP)
-Debug.AddCard(10051531,1,1,LOCATION_MZONE,2,POS_FACEUP)
-Debug.AddCard(10051531,1,1,LOCATION_MZONE,2,POS_FACEUP)
-Debug.AddCard(10051531,1,1,LOCATION_MZONE,2,POS_FACEUP)
-Debug.AddCard(10051531,1,1,LOCATION_MZONE,2,POS_FACEUP)
-Debug.AddCard(10051531,1,1,LOCATION_MZONE,2,POS_FACEUP)
-Debug.AddCard(10051531,1,1,LOCATION_MZONE,2,POS_FACEUP)
-Debug.AddCard(10051531,1,1,LOCATION_MZONE,2,POS_FACEUP)
-Debug.AddCard(10051531,1,1,LOCATION_MZONE,2,POS_FACEUP)
-Debug.AddCard(10051531,1,1,LOCATION_MZONE,2,POS_FACEUP)
-Debug.AddCard(10051531,1,1,LOCATION_MZONE,2,POS_FACEUP)
+Debug.AddCard(10053531,1,1,LOCATION_MZONE,2,POS_FACEUP_ATTACK)
+Debug.AddCard(10053531,1,1,LOCATION_MZONE,2,POS_FACEUP)
+Debug.AddCard(10053531,1,1,LOCATION_MZONE,2,POS_FACEUP)
+Debug.AddCard(10053531,1,1,LOCATION_MZONE,2,POS_FACEUP)
+Debug.AddCard(10053531,1,1,LOCATION_MZONE,2,POS_FACEUP)
+Debug.AddCard(10053531,1,1,LOCATION_MZONE,2,POS_FACEUP)
+Debug.AddCard(10053531,1,1,LOCATION_MZONE,2,POS_FACEUP)
+Debug.AddCard(10053531,1,1,LOCATION_MZONE,2,POS_FACEUP)
 
-Debug.AddCard(10051411,1,1,LOCATION_MZONE,0,POS_FACEUP_ATTACK)
-Debug.AddCard(10051411,1,1,LOCATION_MZONE,0,POS_FACEUP)
-Debug.AddCard(10051411,1,1,LOCATION_MZONE,0,POS_FACEUP)
-Debug.AddCard(10051411,1,1,LOCATION_MZONE,0,POS_FACEUP)
+Debug.AddCard(10051231,1,1,LOCATION_MZONE,0,POS_FACEUP_ATTACK)
+Debug.AddCard(10051231,1,1,LOCATION_MZONE,0,POS_FACEUP)
+Debug.AddCard(10051231,1,1,LOCATION_MZONE,0,POS_FACEUP)
+Debug.AddCard(10051231,1,1,LOCATION_MZONE,0,POS_FACEUP)
+Debug.AddCard(10051231,1,1,LOCATION_MZONE,0,POS_FACEUP)
+Debug.AddCard(10051231,1,1,LOCATION_MZONE,0,POS_FACEUP)
+Debug.AddCard(10051231,1,1,LOCATION_MZONE,0,POS_FACEUP)
+Debug.AddCard(10051231,1,1,LOCATION_MZONE,0,POS_FACEUP)
 
-Debug.AddCard(10051521,1,1,LOCATION_MZONE,4,POS_FACEUP_ATTACK)
-Debug.AddCard(10051521,1,1,LOCATION_MZONE,4,POS_FACEUP)
-Debug.AddCard(10051521,1,1,LOCATION_MZONE,4,POS_FACEUP)
-Debug.AddCard(10051521,1,1,LOCATION_MZONE,4,POS_FACEUP)
-Debug.AddCard(10051521,1,1,LOCATION_MZONE,4,POS_FACEUP)
-Debug.AddCard(10051521,1,1,LOCATION_MZONE,4,POS_FACEUP)
+Debug.AddCard(10052321,1,1,LOCATION_MZONE,4,POS_FACEUP_ATTACK)
+Debug.AddCard(10052321,1,1,LOCATION_MZONE,4,POS_FACEUP)
+Debug.AddCard(10052321,1,1,LOCATION_MZONE,4,POS_FACEUP)
+Debug.AddCard(10052321,1,1,LOCATION_MZONE,4,POS_FACEUP)
+Debug.AddCard(10052321,1,1,LOCATION_MZONE,4,POS_FACEUP)
+Debug.AddCard(10052321,1,1,LOCATION_MZONE,4,POS_FACEUP)
 
-Debug.AddCard(10054521,1,1,LOCATION_SZONE,1,POS_FACEUP)
-Debug.AddCard(10055513,1,1,LOCATION_SZONE,3,POS_FACEUP)
+Debug.AddCard(10054212,1,1,LOCATION_SZONE,1,POS_FACEUP)
+Debug.AddCard(10055311,1,1,LOCATION_SZONE,3,POS_FACEUP)
 
-Debug.AddCard(10054621,0,0,LOCATION_DECK,0,POS_FACEDOWN)
-Debug.AddCard(10054621,0,0,LOCATION_DECK,0,POS_FACEDOWN)
-Debug.AddCard(10054621,0,0,LOCATION_DECK,0,POS_FACEDOWN)
-Debug.AddCard(10054621,0,0,LOCATION_DECK,0,POS_FACEDOWN)
 Debug.AddCard(10054621,0,0,LOCATION_DECK,0,POS_FACEDOWN)
 Debug.AddCard(10054621,0,0,LOCATION_DECK,0,POS_FACEDOWN)
 Debug.AddCard(10054621,0,0,LOCATION_DECK,0,POS_FACEDOWN)
@@ -87,32 +81,40 @@ Debug.AddCard(10054621,1,1,LOCATION_DECK,0,POS_FACEDOWN)
 Debug.AddCard(10054621,1,1,LOCATION_DECK,0,POS_FACEDOWN)
 Debug.AddCard(10054621,1,1,LOCATION_DECK,0,POS_FACEDOWN)
 
-Debug.AddCard(10051631,0,0,LOCATION_MZONE,2,POS_FACEUP_ATTACK)
-Debug.AddCard(10051622,0,0,LOCATION_MZONE,2,POS_FACEUP)
+Debug.AddCard(10053411,0,0,LOCATION_MZONE,2,POS_FACEUP_ATTACK)
+Debug.AddCard(10053411,0,0,LOCATION_MZONE,2,POS_FACEUP)
+Debug.AddCard(10053411,0,0,LOCATION_MZONE,2,POS_FACEUP)
+Debug.AddCard(10053411,0,0,LOCATION_MZONE,2,POS_FACEUP)
 
-Debug.AddCard(10051631,0,0,LOCATION_HAND,0,POS_FACEDOWN)
-Debug.AddCard(10051621,0,0,LOCATION_HAND,0,POS_FACEDOWN)
-Debug.AddCard(10051632,0,0,LOCATION_HAND,0,POS_FACEDOWN)
-Debug.AddCard(10055112,0,0,LOCATION_HAND,0,POS_FACEDOWN)
-Debug.AddCard(10054611,0,0,LOCATION_HAND,0,POS_FACEDOWN)
-Debug.AddCard(10054621,0,0,LOCATION_HAND,0,POS_FACEDOWN)
+Debug.AddCard(10054421,0,0,LOCATION_HAND,0,POS_FACEDOWN)
+Debug.AddCard(10054321,0,0,LOCATION_HAND,0,POS_FACEDOWN)
+Debug.AddCard(10052221,0,0,LOCATION_HAND,0,POS_FACEDOWN)
+Debug.AddCard(10052331,0,0,LOCATION_HAND,0,POS_FACEDOWN)
 
 Debug.ReloadFieldEnd()
 
-Debug.ShowHint[[★쿠죠 카렌
-HAHAHAHA~!
-이세계 전생해 최강 무쌍이 되어버린 나
-전사 카렌쨩은 매일 트레이닝을 게을리 하지 않습니다!]]
-Debug.ShowHint[[★앨리스 카터릿
-시노는 다른 금발소녀들이랑 즐겁게 놀고 있구나...]]
-Debug.ShowHint[[★오오미야 시노부
-오늘 앨리스는 뭔가 미스테리하네요]]
+Debug.ShowHint[[★후다 유미네
+자아... 믿는 겁니다
+부정할 수 없는 BL<이노센트 보이즈 러브>에
+이끌려온 사람들이여...]]
+Debug.ShowHint[[★후다 유미네
+아무것도 부끄러워할 필요 없습니다.
+곧 모든 남자가 남자와 사귀는 세계가 도래할 것입니다.
+BL이야말로 세계의 정도]]
+Debug.ShowHint[[★후다 유미네
+자, 있는 그대로의 최애를 향한 생각을 풀어내는 것입니다
+이 유미네 교단에서 동인지를 발산해 이 에토와리아를
+누구나가 사랑에 정직하게 될 수 있는 세계를 만드는겁니다]]
 Debug.ShowHint[[이번 턴 안에 상대 배틀 크리에메이트를 전부 쓰러트리세요]]
 
 local e0=Effect.CreateEffect(tc)
 e0:SetType(EFFECT_TYPE_SINGLE)
 e0:SetCode(EFFECT_CANNOT_TRIGGER)
 tc:RegisterEffect(e0)
+local e5=Effect.CreateEffect(c)
+e5:SetType(EFFECT_TYPE_SINGLE)
+e5:SetCode(EFFECT_CANNOT_TRIGGER)
+c:RegisterEffect(e5)
 local e1=Effect.CreateEffect(c)
 e1:SetType(EFFECT_TYPE_SINGLE)
 e1:SetCode(EFFECT_UPDATE_DEFENSE)
@@ -150,6 +152,18 @@ local tc=g:GetFirst()
 end)
 c:RegisterEffect(e3)
 
+local e4=Effect.CreateEffect(c)
+e4:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+e4:SetProperty(EFFECT_FLAG_DELAY)
+e4:SetRange(LOCATION_EMZONE)
+e4:SetCountLimit(1)
+e4:SetCode(EVENT_TO_GRAVE)
+e4:SetOperation(function (e,tp,eg,ep,ev,re,r,rp)
+local g=Duel.GetMatchingGroup(nil,tp,LOCATION_GRAVE,0,nil)
+Duel.SendtoDeck(g,nil,-2,REASON_RULE)
+end)
+c:RegisterEffect(e4)
+
 local e9=Effect.CreateEffect(c)
 e9:SetCategory(CATEGORY_DECKDES)
 e9:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
@@ -180,29 +194,43 @@ local e4=Effect.GlobalEffect()
 	e4:SetCountLimit(1)
 	e4:SetOperation(function(e,tp,eg,ep,ev,re,r,rp)
 if Duel.GetMatchingGroupCount(nil,tp,0,LOCATION_MZONE,nil)==1 then
-Debug.ShowHint[[★마츠바라 호노카
-이겼어! 카렌쨩
-자, 카렌쨩에게는 내가 만든 산더미같은 스콘 대서비스~]]
-Debug.ShowHint[[★카렌 쿠죠
-너무 많습니다!]]
-Debug.ShowHint[[★오오미야 시노부
-저... 저는 금발 소녀를 따라간게 아니라
-클레어쨩에게 금발 한정 소환을 부탁하러...]]
-Debug.ShowHint[[★앨리스 카터릿
-시노는 혹시 금발이면 누구든지 좋아하는거야...?]]
-Debug.ShowHint[[★카렌 쿠죠
-Oh, my god!!
-앨리스가 다크 앨리스가 되어버렸습니다!!]]
+Debug.ShowHint[[★이케타니 노노
+저기 죄송한데요, 타마키 선배
+후다후다 선배랑 만든 BL 게임에서
+'젊디젊은 남자 둘이 화장실에서 뭔가를...']]
+Debug.ShowHint[[★이케타니 노노
+이게 BL 묘사인가요?
+화장실에서 뭘 하나요?]]
+Debug.ShowHint[[★혼다 타마키
+묻지마!!!!....
+나도 잘은 모르지만 남자 둘이서...
+소곤소곤]]
+Debug.ShowHint[[★이케타니 노노
+이, 이런 저속한 게임 못 팔아요!!!
+집에 갈래요!!]]
 Duel.SetLP(1-tp,0)
 else
-Debug.ShowHint[[★마츠바라 호노카
-미.. 미안해 카렌쨩
-할 줄 아는게 공굴리기밖에 없어서...]]
-Debug.ShowHint[[★카렌 쿠죠
-깜짝입니다!
-어디서 나온겁니까?]]
-Debug.ShowHint[[★마츠바라 호노카
-응? 난 계속 카렌쨩 옆에 있었는데?]]
+Debug.ShowHint[[★소라
+대어 대어~♪ 어떤 동인지도 모두 훌륭하네요
+하지만 유미네 선생님 건 아르시브가 보면 큰일이니
+어딘가에 숨겨둬야...]]
+Debug.ShowHint[[★아르시브
+소라님!
+또, 동인지를 읽고 있습니까?
+책이 어지러져있고 남들에게 본보기가... 음?]]
+Debug.ShowHint[[★아르시브
+...........]]
+Debug.ShowHint[[★소라
+아르시브?
+무슨 일이니...?
+아르시브~~~!!]]
+Debug.ShowHint[[필두신관 아르시브는 순수하트의 소유자였습니다
+그녀는 동인지의 내용을 받아들이지 못하고 실신
+그것에 대한 기억도 잃어버렸다고 합니다]]
+Debug.ShowHint[[그리고 아르시브를 무참히 녹아웃한 여신님의 동인지는
+신전 금서고에 봉인되어
+필두신관 고로시(아르시브 슬레이어)라고 불려 구전되었고
+신전에 봉인되어 최종병기로서 전설이 되었다고 합니다]]
 Duel.SetLP(tp,0) end
 end)
 Duel.RegisterEffect(e4,0)
